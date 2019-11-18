7d92258f9c7589caa67b1bf99b8c3dac1c84d021
ylgrgyq <alven.gr@gmail.com>
2019-11-18 10:30:44 +0800

    (fix) tests

--- filter-service-core/src/test/java/cn/leancloud/filter/service/ConfigurationTest.java
+++ filter-service-core/src/test/java/cn/leancloud/filter/service/ConfigurationTest.java
@@ -1,14 +1,36 @@
 package cn.leancloud.filter.service;
 
-import cn.leancloud.filter.service.metrics.MetricsService;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.Test;
 
-public class ConfigurationTest {
-    @Test
-    public void testA() throws Exception{
-        Configuration.initConfiguration("/Users/guorui/projects/mine/filter-service/filter-service-core/src/main/resources/configuration.yaml");
-        System.out.println("asdf " + Configuration.spec());
+import java.time.Duration;
 
-        System.out.println(MetricsService.class.getName());
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ConfigurationTest {
+
+    @Test
+    public void testEnsureInitialization() {
+        testEnsureInitializationForMethod(Configuration::purgeFilterInterval);
+        testEnsureInitializationForMethod(Configuration::maxHttpConnections);
+        testEnsureInitializationForMethod(Configuration::maxHttpRequestLength);
+        testEnsureInitializationForMethod(Configuration::defaultRequestTimeout);
+        testEnsureInitializationForMethod(Configuration::channelOptions);
+        testEnsureInitializationForMethod(Configuration::spec);
+    }
+
+    private void testEnsureInitializationForMethod(ThrowingCallable shouldRaiseThrowable){
+        assertThatThrownBy(shouldRaiseThrowable)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("configuration is not initialized");
+    }
+
+    @Test
+    public void testNormalConfigurationFile() throws Exception {
+        Configuration.initConfiguration("src/test/resources/testing-configuration.yaml");
+        assertThat(Configuration.purgeFilterInterval()).isEqualTo(Duration.ofMillis(300));
+        assertThat(Configuration.maxHttpConnections()).isEqualTo(1000);
+        assertThat(Configuration.maxHttpRequestLength()).isEqualTo(10 * 1024 * 1024);
     }
 }
