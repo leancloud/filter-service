f8c0fd017b071abab58da89682d23e1f08de010e
ylgrgyq <alven.gr@gmail.com>
2019-12-08 15:30:23 +0800

    (fix) typo

--- filter-service-core/src/main/java/cn/leancloud/filter/service/Configuration.java
+++ filter-service-core/src/main/java/cn/leancloud/filter/service/Configuration.java
@@ -212,7 +212,6 @@ public final class Configuration {
 
     @JsonProperty("triggerPersistenceCriteria")
     public void setPersistenceCriteria(@Nullable List<TriggerPersistenceCriteria> criteriaList) {
-        System.out.println("sd " + criteriaList);
         if (criteriaList == null) {
             criteriaList = Collections.emptyList();
         }
